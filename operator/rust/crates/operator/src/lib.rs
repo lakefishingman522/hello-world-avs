3a441ff61d57ff2d1a3d4362b2746d04fbb2e4ea
lakefishingman522 <calvingill89@gmail.com>
2024-07-04 00:10:40 +0900

    first commit

--- operator/rust/crates/operator/src/lib.rs
+++
@@ -1,8 +0,0 @@
-// Start creating tasks and respond appropriately
-
-/// Create createNewTask at regular intervals with random task names
-pub mod spam_tasks;
-/// Register Operator and monitor for NewTaskCreated event
-pub mod start_operator;
-/// test utils
-pub mod test_utils;
